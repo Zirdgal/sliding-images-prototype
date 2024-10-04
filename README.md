f39d76a78ae3a4ec3c45f806e87293ad9bda7632
Zirdgal <zirdman@gmail.com>
2024-10-04 19:34:34 +0300

    finished

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-A
+How to use this:
+
+1 )  Add images you want to show in the `img` folder
+
+2 ) go to `index.html` and add the <img> tags with as many images u want to show.
+    Examples:
+        <img class="sliding-img" id="img1" src="./img/1.png">
+        <img class="sliding-img" id="img2" src="./img/2.png" style="display: none;">
+    All of them should use class="sliding-img" and the IDs can be self prefrence but its simpler to use numbered IDs, All but the first <img> should use style="display: none;"
+
+3 ) go to `app.js` and the const Images area.
+    Add your images with the IDs u used in the <img>'s
+    Example: 
+        const images = [
+            img1 = { id: "img1" },
+            ^          ^
+            ^          The value of this should be the ID of your <img>
+            This is just a variable with the name img1
+
+            // Repeat for all Images u have
+        ];
--- app.js
+++ app.js
@@ -4,36 +4,49 @@
 // Array with all of the images in HTML
 const images = [
     img1 = { id: "img1" },
-    img2 = { id: "img2" }
+    img2 = { id: "img2" },
+    img3 = { id: "img3"}
+    // Continue here
 ];
 
-// A tracker so the code knows which image it is on
-let currentImg = 0;
 
 document.addEventListener("DOMContentLoaded", function() { // When webpage loaded
 
-    for (let i = 0; i < 99999; i++) { // bassically an infinite loop
-        setInterval( // Every 5000 ms
-            function slideImages() { // do this function
+    // A tracker so the code knows which image it is on
+    let currentImg = 0;
+    
 
-                if ( currentImg === (images.length - 1)) { // if the current image is at the end
-                    
+    setInterval( // Every 5000 ms
+        function slideImages() { // do this function
+
+            if ( currentImg === (images.length - 1)) { // if the current image is at the end
+                
+                document.getElementById(images[currentImg].id).style.animation = "fadeOut 1s"; // play animation to make it go away
+                setTimeout(() => { // wait for the animation to play...
                     document.getElementById(images[currentImg].id).style.display = "none"; // change the actual <img> in the webpage to hidden
-            
+
                     document.getElementById(images[0].id).style.display = "block"; //change the actual <img> in the webpage to show
-        
-                    currentImg = 0;
-                } else {
-                    console.log(images.length);
+                    document.getElementById(images[0].id).style.animation = "fadeIn 1s";// play animation to make it show up
+                    currentImg = 0; // current Item in list
+                }, 900);
+            } else {
+                document.getElementById(images[currentImg].id).style.animation = "fadeOut 1s"; // play animation to make it go away
+                setTimeout(() => { // wait for the animation to play...
                     document.getElementById(images[currentImg].id).style.display = "none"; // change the actual <img> in the webpage to hidden
-            
-                    document.getElementById(images[currentImg++].id).style.display = "block";
+                    document.getElementById(images[currentImg + 1].id).style.display = "block"; // change the new <img> in the webpage to viewable!!!!
+                    document.getElementById(images[currentImg + 1].id).style.animation = "fadeIn 1s"; // play animation to make it show up
+    
+                    currentImg++; // add one to the current item in the list
+                }, 900); 
+            }
+        },
+    5000);
 
-                    currentImg++;
-                }
-            },
-        1000);
-    };
+
+
+
+
+
 });
 
 
